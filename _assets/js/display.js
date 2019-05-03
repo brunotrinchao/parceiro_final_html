14a1b0d187dd5d21882faf09c7984336207cac20
Bruno Trinchao <brunotrinchao@gmail.com>
2019-05-03 15:04:09 -0300

    Arquivos

--- _assets/js/display.js
+++ _assets/js/display.js
@@ -13,28 +13,43 @@
         loadStop: function(target) {
             Swal.close();
         },
-        showAlert: function(json) {
+        showAlert: function(msg, callBackAfter) {
             Swal.fire({
                 type: 'warning',
                 title: 'Atenção',
                 html: msg.replace("\n", "<br>"),
                 showConfirmButton: true,
+                onAfterClose: function() {
+                    if (typeof callBackAfter === "function") {
+                        callBackAfter.call(this);
+                    }
+                }
             });
         },
-        showSuccess: function(msgn) {
+        showSuccess: function(msg, callBackAfter) {
             Swal.fire({
                 type: 'success',
                 title: 'Sucesso',
                 html: msg.replace("\n", "<br>"),
                 showConfirmButton: true,
+                onAfterClose: function() {
+                    if (typeof callBackAfter === "function") {
+                        callBackAfter.call(this);
+                    }
+                }
             });
         },
-        showError: function(msg) {
+        showError: function(msg, callBackAfter) {
             Swal.fire({
                 type: 'error',
                 title: 'Erro',
                 html: msg.replace(/\n/g, "<br />"),
                 showConfirmButton: true,
+                onAfterClose: function() {
+                    if (typeof callBackAfter === "function") {
+                        callBackAfter.call(this);
+                    }
+                }
             });
         }
     }
