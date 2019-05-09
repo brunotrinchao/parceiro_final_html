631d29825ae4980df681c7fbf3257e93278cec56
Bruno Trinchao <brunotrinchao@gmail.com>
2019-05-09 18:25:16 -0300

    Ajuste no tamanho do container

--- _assets/js/functions.js
+++ _assets/js/functions.js
@@ -521,3 +521,15 @@ function ToSeoUrl(url) {
         .replace(/^-*/, '') // Remove starting dashes
         .replace(/-*$/, '');
 }
+
+function getBase64(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function() {
+        return reader.result;
+    };
+    reader.onerror = function(error) {
+        //   console.log('Error: ', error);
+        return;
+    };
+}
