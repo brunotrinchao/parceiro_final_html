0844b2e1a3f4a84e7e72ee8aabb9414050969549
brunotrinchao <brunotrinchao@gmail.com>
2019-05-08 22:46:13 -0300

    Arquivos

--- _assets/js/functions.js
+++ _assets/js/functions.js
@@ -512,23 +512,16 @@ function doModal(placementId, heading, formContent, strSubmitFunc, btnText) {
 }
 
 function ToSeoUrl(url) {
-
-    // make the url lowercase         
-    var encodedUrl = url.toString().toLowerCase();
-
-    // replace & with and           
-    encodedUrl = encodedUrl.split(/\&+/).join("-e-")
-
-    // remove invalid characters 
-    encodedUrl = encodedUrl.split(/[^a-z0-9]/).join("-");
-
-    // remove duplicates 
-    encodedUrl = encodedUrl.split(/-+/).join("-");
-
-    // trim leading & trailing characters 
-    encodedUrl = encodedUrl.trim('-');
-
-    return encodedUrl;
+    return url.toString()               // Convert to string
+        .normalize('NFD')               // Change diacritics
+        .replace(/[\u0300-\u036f]/g,'') // Remove illegal characters
+        .replace(/\s+/g,'-')            // Change whitespace to dashes
+        .toLowerCase()                  // Change to lowercase
+        .replace(/&/g,'-and-')          // Replace ampersand
+        .replace(/[^a-z0-9\-]/g,'')     // Remove anything that is not a letter, number or dash
+        .replace(/-+/g,'-')             // Remove duplicate dashes
+        .replace(/^-*/,'')              // Remove starting dashes
+        .replace(/-*$/,''); 
 }
 
 function getBase64(file) {
